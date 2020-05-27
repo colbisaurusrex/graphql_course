99b1b2be32da1aeae0a8c976a612cd601e317ec7
Colby Barthelmess <colbybarthelmess@waypointbuilding.com>
2020-05-27 10:17:47 -0400

    navbar; networkInterface

--- Section 2/client/index.js
+++ Section 2/client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
-import ApolloClient from 'apollo-client';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import './style/style.css'
 import App from './components/App.jsx';
@@ -9,11 +9,19 @@ import SongList from './components/SongList.jsx';
 import SongCreate from './components/SongCreate.jsx';
 import SongDetail from './components/SongDetail.jsx';
 
+const networkInterface = createNetworkInterface({
+  uri: '/graphql',
+  opts: {
+    credentials: 'same-origin'
+  }
+});
+
 const client = new ApolloClient({
   /*
     "o" short for object. Every piece of data fetched by Apollo client is ran through this function. Use the id to uniquely identify the record. This only works when all the ids of the entities in our application are unique.
   */
-  dataIdFromObject: o => o.id
+  dataIdFromObject: o => o.id,
+  networkInterface
 });
 
 const Root = () => {
